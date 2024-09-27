9a524557073c6780242eea48cf936b8d30eff6f0
Jose luis <spjjoe2@yahoo.es>
2024-09-27 13:05:44 +0200

    autenticacion con token

---
+++ 38_microservicio_formacion_acceso_oauth2/src/main/java/model/TokenResponse.java
@@ -0,0 +1,24 @@
+package model;
+
+public class TokenResponse {
+	private String access_token;
+
+	public TokenResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TokenResponse(String access_token) {
+		super();
+		this.access_token = access_token;
+	}
+
+	public String getAccess_token() {
+		return access_token;
+	}
+
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	
+}
